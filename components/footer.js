05999e610562fedaa62f10cde9639f028a341578
Abdo <abd.nh25@gmail.com>
2022-07-26 02:26:57 +0300

    Add repo link to footer

--- components/footer.js
+++ components/footer.js
@@ -24,8 +24,7 @@ const Footer = () => {
           </a>
         </li>
         <li>
-          {/* TODO: add repo link */}
-          <a id="source" href="">
+          <a id="source" href="https://github.com/abdnh/nextjs-blog">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
